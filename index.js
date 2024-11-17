aa1a8bb974f895f8ad1db9d0fbb3b13ffdd2d01e
jmmb8 <jorgemanuel1986@gmail.com>
2024-11-16 21:03:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Registrar, Leer } = require('./operaciones');
+
+const argumentos = process.argv.slice(2);
+const operacion = argumentos[0];
+
+if (operacion === 'registrar') {
+  if (argumentos.length < 6) { 
+    console.log('Faltan argumentos');
+    return;
+  }
+  Registrar(
+    argumentos[1],
+    argumentos[2],
+    argumentos[3],
+    argumentos[4],
+    argumentos[5]
+  );
+} else if (operacion === 'leer') {
+  Leer();
+}
---
+++ operaciones.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const Registrar = (nombre, edad, tipo, color, enfermedad) => {
+  let citas = [];
+  if (fs.existsSync("./citas.json")) {
+    let data = fs.readFileSync("./citas.json", "utf8");
+    citas = JSON.parse(data);
+  }
+
+  let registro = {
+    nombre,
+    edad,
+    tipo,
+    color,
+    enfermedad,
+  };
+  citas.push(registro);
+  fs.writeFileSync("./citas.json", JSON.stringify(citas));
+};
+
+const Leer = () => {
+  if (fs.existsSync("./citas.json")) {
+    let data = fs.readFileSync("./citas.json", "utf8");
+    citas = JSON.parse(data);
+    if (citas.length == 0) {
+      console.log("No hay registros en el archivo citas.json");
+    } else {
+      console.log(citas);
+    }
+  } else {
+    console.log("No existe el archivo citas.json");
+  }
+};
+
+module.exports = {
+  Registrar,
+  Leer,
+};
